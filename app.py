0f266a397d6a8105151e588d1d886467c4e664a3
thushali <thushali@gmail.com>
2022-10-06 21:31:18 +0530

    Version 2.0

--- app.py
+++ app.py
@@ -1,50 +1,45 @@
-from flask import Flask, render_template, request
-from keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from PIL import Image
-import numpy as np
-import tensorflow as tf
-import cv2
 
-app= Flask(__name__)
+from flask import Flask, request, render_template
 
-dic = {'Apple___Apple_scab': 0, 'Apple___Black_rot': 1, 'Apple___Cedar_apple_rust': 2, 'Apple___healthy': 3, 'Blueberry___healthy': 4, 'Cherry_(including_sour)___Powdery_mildew': 5, 'Cherry_(including_sour)___healthy': 6, 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot': 7, 'Corn_(maize)___Common_rust_': 8, 'Corn_(maize)___Northern_Leaf_Blight': 9, 'Corn_(maize)___healthy': 10, 'Grape___Black_rot': 11, 'Grape___Esca_(Black_Measles)': 12, 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)': 13, 'Grape___healthy': 14, 'Orange___Haunglongbing_(Citrus_greening)': 15, 'Peach___Bacterial_spot': 16, 'Peach___healthy': 17, 'Pepper,_bell___Bacterial_spot': 18, 'Pepper,_bell___healthy': 19, 'Potato___Early_blight': 20, 'Potato___Late_blight': 21, 'Potato___healthy': 22, 'Raspberry___healthy': 23, 'Soybean___healthy': 24, 'Squash___Powdery_mildew': 25, 'Strawberry___Leaf_scorch': 26, 'Strawberry___healthy': 27, 'Tomato___Bacterial_spot': 28, 'Tomato___Early_blight': 29, 'Tomato___Late_blight': 30, 'Tomato___Leaf_Mold': 31, 'Tomato___Septoria_leaf_spot': 32, 'Tomato___Spider_mites Two-spotted_spider_mite': 33, 'Tomato___Target_Spot': 34, 'Tomato___Tomato_Yellow_Leaf_Curl_Virus': 35, 'Tomato___Tomato_mosaic_virus': 36, 'Tomato___healthy': 37}
 
-model = load_model('train1.h5')
+app = Flask(__name__)
 
-model.make_predict_function()
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
 
-def predict_label(img_path):
-    #i = cv2.imread(img_path)
-    #i = tf.io.decode_jpeg(i, channels=3)
-    #i = cv2.resize(i,(224,224))
-    #i = i/225.0
-    i = image.load_img(img_path,target_size=(224,224))
-    i = image.img_to_array(i)/225.0
-    i = i.reshape(1,224,224,3)
-    p = model.predict(i)
-    no = max(p[0])
-    indx = list(p[0]).index(no) 
-    result= dict((new_val,new_k) for new_k,new_val in dic.items()).get(indx)
-    return result
+cred = credentials.Certificate("C:/Users/Ishan/OneDrive/Desktop/firebase/Scripts/ServiceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
-#routes
+db = firestore.client()
 
-@app.route("/", methods=['GET','POST'])
-def main():
-    return render_template("index.html")
 
-@app.route("/submit",methods=['GET','POST'])
-def get_pred():
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/signup", methods=["POST"])
+def signup():
     if request.method == 'POST':
-        img = request.files['my_image']
-        img_path = 'static/'+img.filename
-        img.save(img_path)
-        p = predict_label(img_path)
+        user_data = [x for x in request.form.values()]
+        user = auth.create_user(
+            email=user_data[1],
+            uid=user_data[0],
+            #phone_number=user_data[3],
+            password=user_data[2],
+            display_name=user_data[0],
+            disabled=False)
 
-    return render_template("index.html", prediction = p , img_path=img_path)
 
-if __name__ == '__main__':
-    app.run(debug = True)
+        doc_ref = db.collection(user.uid).document('User Details')
+        doc_ref.set({
+            'name':user.display_name,
+            'email': user.email,
+            #'phone_number':user.phone_number
+        })
+    return render_template("home.html")
 
-
+if __name__ == "__main__":
+    app.run()
---
+++ test.py
@@ -0,0 +1,198 @@
+from urllib import response
+from flask import Flask, render_template, request, Markup
+from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from utils.disease import disease_dic
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import cv2
+import datetime
+import json
+
+from fileinput import filename
+from heapq import merge
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate(
+    "./ServiceAccountKey.json")
+farmers = firebase_admin.initialize_app(
+    cred, {'storageBucket': 'agri-app-af9c7.appspot.com', 'databaseURL': 'https://agri-app-af9c7-default-rtdb.firebaseio.com'})
+
+depcred = credentials.Certificate(
+    "./agri-department-firebase-adminsdk-759dj-04fc05ee9e.json")
+department = firebase_admin.initialize_app(depcred, {
+                                           'storageBucket': "agri-department.appspot.com", 'databaseURL': "https://agri-department-default-rtdb.firebaseio.com"}, name="other")
+
+
+# import firebase_admin.db
+# db = firebase_admin.db.reference(path='/', app=farmers, url="https://agri-app-af9c7-default-rtdb.firebaseio.com/")
+
+# db = farmers.db
+# from firebase_admin import db
+# db = farmers.database('https://secondary_db_url.firebaseio.com')
+# firebase_admin.get_app(farmers)
+# # db = firebase_admin.db
+# storage = farmers.storage
+
+#depdb = firebase_admin.db.reference(path='/', app=department, url="https://agri-app-af9c7-default-rtdb.firebaseio.com/")
+# depstorage = department.storage
+
+
+app = Flask(__name__)
+
+dic = {'Apple___Apple_scab': 0, 'Apple___Black_rot': 1, 'Apple___Cedar_apple_rust': 2, 'Apple___healthy': 3, 'Blueberry___healthy': 4, 'Cherry_(including_sour)___Powdery_mildew': 5, 'Cherry_(including_sour)___healthy': 6, 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot': 7, 'Corn_(maize)___Common_rust_': 8, 'Corn_(maize)___Northern_Leaf_Blight': 9, 'Corn_(maize)___healthy': 10, 'Grape___Black_rot': 11, 'Grape___Esca_(Black_Measles)': 12, 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)': 13, 'Grape___healthy': 14, 'Orange___Haunglongbing_(Citrus_greening)': 15, 'Peach___Bacterial_spot': 16, 'Peach___healthy': 17,
+       'Pepper,_bell___Bacterial_spot': 18, 'Pepper,_bell___healthy': 19, 'Potato___Early_blight': 20, 'Potato___Late_blight': 21, 'Potato___healthy': 22, 'Raspberry___healthy': 23, 'Soybean___healthy': 24, 'Squash___Powdery_mildew': 25, 'Strawberry___Leaf_scorch': 26, 'Strawberry___healthy': 27, 'Tomato___Bacterial_spot': 28, 'Tomato___Early_blight': 29, 'Tomato___Late_blight': 30, 'Tomato___Leaf_Mold': 31, 'Tomato___Septoria_leaf_spot': 32, 'Tomato___Spider_mites Two-spotted_spider_mite': 33, 'Tomato___Target_Spot': 34, 'Tomato___Tomato_Yellow_Leaf_Curl_Virus': 35, 'Tomato___Tomato_mosaic_virus': 36, 'Tomato___healthy': 37}
+
+model = load_model('./train1.h5')
+
+model.make_predict_function()
+
+
+def predict_label(img_path):
+    #i = cv2.imread(img_path)
+    #i = tf.io.decode_jpeg(i, channels=3)
+    #i = cv2.resize(i,(224,224))
+    #i = i/225.0
+    i = image.load_img(img_path, target_size=(224, 224))
+    i = image.img_to_array(i)/225.0
+    i = i.reshape(1, 224, 224, 3)
+    p = model.predict(i)
+    no = max(p[0])
+    print(no)
+    indx = list(p[0]).index(no)
+    result = dict((new_val, new_k) for new_k, new_val in dic.items()).get(indx)
+    return result
+
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=['GET', 'POST'])
+def get_pred():
+    if request.method == 'POST':
+        img = request.files['my_image']
+        #img_path = 'C:/Users/USER/Desktop/firebase/./static/'+img.filename
+        #img_path= './static/'+img.filename
+        img_pathn = 'static/'+img.filename
+        img_path = './static/'+img.filename
+        img.save(img_path)
+        fileName = img_path
+        bucket = storage.bucket(app=farmers)
+        p = predict_label(img_path)
+        p = Markup(str(disease_dic[p]))
+
+    return render_template("index.html", prediction=p, img_pathn=img_pathn)
+
+
+@app.route("/logged_in", methods=['POST'])
+def button():
+    global current_user
+    current_user = request.form.get("custId")
+    ref_ = db.reference('Farmers/' + current_user + '/userdetails/username')
+    username_ = ref_.get()
+    posts = db.reference('Farmers/' + current_user + '/submissions').get()
+    if posts is not None:
+        vals = list(posts.values())
+        for i in range(len(vals)):
+            vals[i]["filename"] = "https://storage.googleapis.com/agri-app-af9c7.appspot.com/static/" + vals[i]["filename"]
+        posts = json.dumps(vals)
+    else:
+        posts = {}    
+    print(posts)
+    return render_template("home.html" , x = username_.upper() , posts = posts)
+
+
+@app.route("/logged_inD", methods=['POST'])
+def dep():
+    global current_depuser
+    current_depuser = request.form.get("depcustId")
+    ref_ = db.reference('Department/' + current_depuser + '/userdetails/username',app=department, url="https://agri-department-default-rtdb.firebaseio.com/")
+    username_ = ref_.get()
+    current_depuser = request.form.get("depcustId")
+    return render_template("department.html", x = username_)
+
+
+@app.route("/uploaded", methods=['POST'])
+def loggedD():
+    files = request.files.getlist('files[]')
+    dname = request.form['fname']
+    print(type(dname))
+    for file in files:
+        img_path = './static/upload/' + \
+            dname+'__'+file.filename
+        temp = dname+'__'+file.filename
+        file.save(img_path)
+
+        fileName = img_path
+        bucket = storage.bucket(app=department)
+        blob = bucket.blob(fileName)
+        blob.upload_from_filename(fileName)
+        blob.make_public()
+        print(current_depuser)
+        path = 'Department/' + current_depuser
+        ref = db.reference(
+            path, app=department, url="https://agri-department-default-rtdb.firebaseio.com/")
+        users_ref = ref.child('submissions')
+        key = (temp).split(".")[0]
+        users_ref.update({key: temp})
+
+    #img = request.files['my_images']
+    # print(request.files)
+    #img_path = 'C:/Users/USER/Desktop/firebase/./static/'+img.filename
+    #img_pathn= 'static/'+img.filename
+    msg = "Files Uploaded Successfully"
+    return render_template("department.html", msg = msg)
+
+
+@app.route("/prediction", methods=['POST'])
+def logged():
+    img = request.files['my_image']
+    img_path = './static/'+img.filename
+    img_pathn ='static/'+img.filename
+    img.save(img_path)
+    fileName = img_path
+    bucket = storage.bucket(app=farmers)
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+    blob.make_public()
+    ref = db.reference('Farmers/' + current_user, app=farmers,
+                       url="https://agri-app-af9c7-default-rtdb.firebaseio.com/")
+    ref_ = db.reference('Farmers/' + current_user + '/userdetails/username',
+                        app=farmers, url="https://agri-app-af9c7-default-rtdb.firebaseio.com/")
+    username_ = ref_.get()
+    print(username_)
+    key = (img.filename).split(".")[0]
+    users_ref = ref.child('submissions/'+key)
+    users_ref.update({"filename": img.filename})
+    p = predict_label(img_path)
+    p = Markup(str(disease_dic[p]))
+    ct = datetime.datetime.now()
+    ct = str(ct)
+    print(ct)
+    users_ref.update({"prediction": p})
+    users_ref.update({"date": ct})
+    posts = db.reference('Farmers/' + current_user + '/submissions', app=farmers,
+                         url="https://agri-app-af9c7-default-rtdb.firebaseio.com/").get()
+
+    if posts is not None:
+        vals = list(posts.values())
+        for i in range(len(vals)):
+            vals[i]["filename"] = "https://storage.googleapis.com/agri-app-af9c7.appspot.com/static/" + vals[i]["filename"]
+        posts = json.dumps(vals)
+    else:
+        posts = {}
+    #vals = list(posts.values())
+    
+    
+    return render_template("home.html", prediction=p, img_pathn=img_pathn, x=username_.upper(), posts=posts, background = "/static/navbar.jpg")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
